9ef692f8c511713e2c15a557dfe532f9ea724b01
varad <varadkhonde@gmail.com>
2023-04-26 17:14:06 +0530

    streamlit.py modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-
+from flask import Flask, request , render_template
+from keras.utils import img_to_array
+from defect_detection.config import STATIC_FOLDER_PATH , IMAGE_SIZE, RESIZE_FACTOR
+from defect_detection.utils.prediction import get_predictions
+from defect_detection.utils import helpers
+from werkzeug.utils import secure_filename
+from PIL import Image
+import os
 st.write(
     "Hello "
 )
